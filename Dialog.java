6e50553a3645e8c391394bd61e41112e6be5c627
BravineTheeDeveloper <bravinebwire98@gmail.com>
2023-06-15 17:18:40 +0300

    Dialog

--- Dialog.java
+++ Dialog.java
@@ -1,3 +1,4 @@
+import javax.lang.model.util.ElementScanner6;
 import javax.swing.JOptionPane;
 public class Dialog
 {
@@ -5,18 +6,34 @@ public class Dialog
     {
         int number1, number2, sum;
         double average;
-        String firstNumber = JOptionPane.showInputDialog("Enter the first number: ");
-        String secondNumber = JOptionPane.showInputDialog("Enter the second number: ");
+        int response;
 
-        //Convert numbers from string to numerical
-        number1 = Integer.parseInt(firstNumber);
-        number2 = Integer.parseInt(secondNumber);
+        do{
+            String firstNumber = JOptionPane.showInputDialog("Enter the first number: ");
+            String secondNumber = JOptionPane.showInputDialog("Enter the second number: ");
 
-        //Compute sum and average
-        sum = number1 + number2;
-        average = (double) sum/2; //Typecasting
+            //Convert numbers from string to numerical
+            number1 = Integer.parseInt(firstNumber);
+            number2 = Integer.parseInt(secondNumber);
 
-        //Display the output
-        JOptionPane.showMessageDialog(null, "Sum is: "+sum +"Average is: "+average);
+            //Compute sum and average
+            sum = number1 + number2;
+            average = (double) sum/2; //Typecasting
+
+            //Display the output
+            JOptionPane.showMessageDialog(null, "Sum is: "+sum +" Average is: "+average);
+            response = JOptionPane.showConfirmDialog(null, firstNumber, secondNumber, response);
+        }while(response = JOptionPane.YES_OPTION);
+
+
+        /*int choice = JOptionPane.showOptionDialog(null, "Do you want to continue?","Choice", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, new Object[]{"Yes", "No", "Cancel"}, "Yes");
+        if(choice == JOptionPane.YES_OPTION)
+        {
+            main(null);
+        }
+        else
+        {
+            System.exit(0);
+        }*/
     }
 }
